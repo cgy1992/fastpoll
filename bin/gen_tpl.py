7f6ada6a63344c2d5f469e93133c0a2293d28a6f
teslaNova <terranova@fahr-zur-hoelle.org>
2015-03-28 19:38:41 +0100

    generate template constants via definition files

---
+++ bin/gen_tpl.py
@@ -0,0 +1,58 @@
+import sys
+import io
+import os
+import re
+import argparse
+
+if __name__ == '__main__':
+  consts = [{'name': '', 'definition': ''}]
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', '--input', metavar="tdf", type=str, nargs='+', required=True)
+  parser.add_argument('-o', '--output', metavar="tof", type=str, nargs=1, required=True)
+
+  args = parser.parse_args()
+
+  for tdf_path in args.input:
+    with open(tdf_path, "r", encoding="utf-8") as tdf:
+      lines = tdf.readlines()
+
+      if len(lines) < 1:
+        sys.exit(1)
+
+      in_def = False
+      nl_cnt = 0
+
+      # parse lines 
+      for line in lines:
+        for ch in line:
+          if(in_def == False):
+            if(nl_cnt > 0):
+              nl_cnt = 0
+              consts.append({'name': '', 'definition': ''})
+
+            if(ch != ':'):
+              if(ch not in [' ', '\t', '\n']):
+                consts[-1]['name'] += ch 
+            else:
+              in_def = True
+
+          else:
+            if(ch == '\n'):
+              nl_cnt += 1
+
+              if(nl_cnt == 2):
+                in_def = False
+            else:
+              if(ch not in [' ', '\t', '\n']):
+                consts[-1]['definition'] += ch
+
+      # write template makros
+      with open(args.output[0], "w", encoding="utf-8") as tof:
+        for const in consts:
+          const['definition'] = re.sub('"', '\\"', const['definition'])
+
+          if(len(const['name']) > 0 and len(const['definition']) > 0):
+            tof.write("#define " + const['name'].upper() + " \"" + const['definition'] + "\"" + '\n' * 2);
+
+  sys.exit(0)
