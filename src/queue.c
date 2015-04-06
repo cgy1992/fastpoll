0f42348c144ffa410ccf5796ffeaf528cd31b003
teslaNova <terranova@fahr-zur-hoelle.org>
2015-04-06 15:48:03 +0200

    documentation, mtx, some little adjustments

--- src/queue.c
+++ src/queue.c
@@ -7,20 +7,51 @@
 # include <stdio.h>
 #endif
 
-#define QUEUE_ALLOC_SIZE 10
+#define QUEUE_ALLOC_SIZE 50
 
-void fsp_queue_rearrange(struct queue*);
+/**
+ * (INTERNAL FUNCTION)
+ *
+ * resets front-/back-pointer if too many spaces are unused and
+ * shrinks the queue size if more than one chunk is unused.
+ * 
+ * @param    queue
+ */
+static inline void fsp_queue_rearrange(struct queue*);
 
+/**
+ * initializes the queue 
+ * 
+ * @param    queue
+ */
 void fsp_queue_init(struct queue *q)
 {
   if(q == NULL) {
     return;
   }
 
-  q->front = q->back = q->data = malloc(QUEUE_ALLOC_SIZE * sizeof(void*));
+  q->back = q->data = malloc(QUEUE_ALLOC_SIZE * sizeof(void*));
+
+  if(q->data == NULL) {
+#ifdef FSP_DEBUG
+      printf("(-- ERROR: allocation failed: %s in %s at %d --)\n", __FILE__, __FUNCTION__, __LINE__);
+#endif    
+  }
+
+  q->front = NULL;
   q->size = QUEUE_ALLOC_SIZE;
+
+  pthread_mutex_init(&q->mtx, 0);
 }
 
+/**
+ * free's and the queue.
+ * 
+ * be aware that the queue does NOT free the elements in 
+ * itself since it doesn't know wether the element is allocated or not.
+ * 
+ * @param    queue
+ */
 void fsp_queue_free(struct queue *q)
 {
   if(q == NULL) {
@@ -33,13 +64,25 @@ void fsp_queue_free(struct queue *q)
 
   q->data = q->front = q->back = NULL;
   q->size = 0;
+
+  pthread_mutex_destroy(&q->mtx);
 }
 
+/**
+ * appends the element to the back of the queue. 
+ *
+ * queue may perform some optimizations.
+ * 
+ * @param    queue
+ * @param    element
+ */
 void fsp_queue_push(struct queue *q, void *e)
 {
   if(q == NULL || e == NULL) {
     return;
   }
+
+  pthread_mutex_lock(&q->mtx);
 
   fsp_queue_rearrange(q);
 
@@ -53,51 +96,81 @@ void fsp_queue_push(struct queue *q, void *e)
 #endif
 
     size_t pos_front = q->front - q->data;
-
-    q->data = realloc(q->data, (q->size + QUEUE_ALLOC_SIZE) * sizeof(void*));
     
-    q->back = &q->data[q->size - 1];
-    q->front = &q->data[pos_front];
+    q->data = realloc(q->data, (q->size + QUEUE_ALLOC_SIZE) * sizeof(void*));
 
+    if(q->data == NULL)
+    {
+#ifdef FSP_DEBUG
+      printf("(-- ERROR: allocation failed: %s in %s at %d --)\n", __FILE__, __FUNCTION__, __LINE__);
+#endif
+    }
+    
+    if(q->front != NULL)
+      q->front = &q->data[pos_front];
+
+    q->back = &q->data[q->size - 1];
     q->size += QUEUE_ALLOC_SIZE;
   }
 
-  
-  *q->back = e;
-
-  if(q->front == NULL) // korrekt?!
-  {
-    //printf("\nduh\n");
+  if(q->front == NULL)
     q->front = q->back;
-  }
 
-  q->back++;
+  *q->back++ = e;
+
+  pthread_mutex_unlock(&q->mtx);
 }
 
+/**
+ * returns the front element if there is one, otherwise NULL'll be returned.
+ * 
+ * @param    queue
+ * @return   element
+ */
 void *fsp_queue_pop(struct queue *q)
 {
-  if(q == NULL || q->front == NULL) {
+  if(q == NULL) {
+    return NULL;
+  }
+
+  pthread_mutex_lock(&q->mtx);
+
+  if(q->front == NULL) {
+    pthread_mutex_unlock(&q->mtx);
     return NULL;
   }
 
   void *e = *q->front;
 
-  if(++q->front == q->back) {
+  if(++q->front == q->back)
     q->front = NULL;
-  }
+
+  pthread_mutex_unlock(&q->mtx);
 
   return e;
 }
 
+/**
+ * returns confirmation if queue is empty
+ * 
+ * @param    queue
+ * @return   true if empty or false if not
+ */
 bool fsp_queue_empty(struct queue *q)
 {
   if(q == NULL || q->front == NULL) {
     return true;
   }
 
-  return false;
+  return fsp_queue_size(q) == 0;
 }
 
+/**
+ * returns the element count
+ * 
+ * @param    queue
+ * @return   element count
+ */
 size_t fsp_queue_size(struct queue *q)
 {
   if(q == NULL) {
@@ -107,8 +180,15 @@ size_t fsp_queue_size(struct queue *q)
   return (q->front == NULL ? 0 : (q->back - q->data) - (q->front - q->data));
 }
 
-/* INTERNAL */
-void fsp_queue_rearrange(struct queue *q)
+/**
+ * (INTERNAL FUNCTION)
+ *
+ * resets front-/back-pointer if too many spaces are unused and
+ * shrinks the queue size if more than one chunk is unused.
+ * 
+ * @param    queue
+ */
+static inline void fsp_queue_rearrange(struct queue *q)
 {
   if(q == NULL) {
     return;
@@ -120,7 +200,7 @@ void fsp_queue_rearrange(struct queue *q)
       
       q->back = q->data;
 
-    } else if(q->front - q->data >= (QUEUE_ALLOC_SIZE - 1)) {
+    } else if(q->front - q->data >= (QUEUE_ALLOC_SIZE - 1)) { // TODO: use more suited value 
 #ifdef FSP_DEBUG      
       printf("(unused spaces: %ld, elements in queue: %ld)\n", q->front - q->data, (q->back - q->front));
 #endif
@@ -142,8 +222,19 @@ void fsp_queue_rearrange(struct queue *q)
 #endif
 
     q->data = realloc(q->data, (q->size - (chunks * QUEUE_ALLOC_SIZE)) * sizeof(void*));
-    q->back = &q->data[elements - 1];
     q->size = (q->size - (chunks * QUEUE_ALLOC_SIZE));
+
+    if(q->data == NULL) {
+#ifdef FSP_DEBUG
+      printf("(-- ERROR: allocation failed: %s in %s at %d --)\n", __FILE__, __FUNCTION__, __LINE__);
+#endif
+    }
+
+    if(elements > 0) {
+      q->back = &q->data[elements - 1];
+    } else {
+      q->back = q->data;
+    }
 
     if(q->front != NULL) {
       q->front = q->data;
--- src/queue.h
+++ src/queue.h
@@ -7,17 +7,63 @@
 
 #include "fsp.h"
 
+#include <pthread.h>
+
 struct queue 
 {
   void **data, **front, **back;
   size_t size;
+
+  pthread_mutex_t mtx;
 };
 
+/**
+ * initializes the queue 
+ * 
+ * @param    queue
+ */
 void fsp_queue_init(struct queue*) FSP_UNUSED;
+
+/**
+ * free's and the queue.
+ * 
+ * be aware that the queue does NOT free the elements in 
+ * itself since it doesn't know wether the element is allocated or not.
+ * 
+ * @param    queue
+ */
 void fsp_queue_free(struct queue*) FSP_UNUSED;
 
+/**
+ * appends the element to the back of the queue. 
+ *
+ * queue may perform some optimizations.
+ * 
+ * @param    queue
+ * @param    element
+ */
 void fsp_queue_push(struct queue*, void*) FSP_UNUSED;
+
+/**
+ * returns the front element if there is one, otherwise NULL'll be returned.
+ * 
+ * @param    queue
+ * @return   element
+ */
 void *fsp_queue_pop(struct queue*) FSP_UNUSED;
 
+/**
+ * returns confirmation if queue is empty
+ * 
+ * @param    queue
+ * @return   true if empty or false if not
+ */
 bool fsp_queue_empty(struct queue*) FSP_UNUSED;
+
+/**
+ * returns the element count
+ * 
+ * @param    queue
+ * @return   element count
+ */
 size_t fsp_queue_size(struct queue*) FSP_UNUSED;
